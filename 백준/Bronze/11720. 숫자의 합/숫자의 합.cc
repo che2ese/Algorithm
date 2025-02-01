94bef0633b2ee0ad51a324ce939beb8758f748ef
MS_1129 <141838530+che2ese@users.noreply.github.com>
2025-02-01 20:47:57 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -7,7 +7,7 @@ int main(){
     cin >> num;
     cin.ignore();  // 이전 입력에서 남은 개행 문자 제거
 
-    char input[num + 2];
+    char input[num + 1];
     cin.getline(input, num + 1);
     for(int i = 0; i < num; i++){
         sum += (input[i] - '0');
