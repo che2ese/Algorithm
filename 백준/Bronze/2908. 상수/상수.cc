93b5c91eb63dcf504cc889896c4ade051514a7e0
MS_1129 <141838530+che2ese@users.noreply.github.com>
2025-02-01 21:24:30 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int Change(char* input){
+    return (input[0] - '0') * 100 + (input[1] - '0') * 10 + (input[2] - '0');
+}
+int main() {
+    char input1[4]; 
+    char input2[4]; 
+    char newInput1[4];  
+    char newInput2[4]; 
+
+    cin >> input1 >> input2; 
+ 
+    // 역순으로 배열 복사
+    for (int i = 0; i < 3; i++) {
+        newInput1[i] = input1[2 - i];  
+        newInput2[i] = input2[2 - i];  
+    }
+    newInput1[3] = '\0'; 
+    newInput2[3] = '\0'; 
+
+
+    if(Change(newInput1) > Change(newInput2))
+        cout << Change(newInput1) << endl;
+    else
+        cout << Change(newInput2) << endl;
+}
